ffff8264474c6f77c56b43039abcdfc1d79705e2
Diogo Steinke Deconto <diogo.deconto@pf.up.edu.br>
2025-10-31 09:42:27 -0300

    FRONT - Cadastrar e Listar produtos com fetch

--- Ecommerce/front/src/components/pages/produto/CadastrarProduto.tsx
+++ Ecommerce/front/src/components/pages/produto/CadastrarProduto.tsx
@@ -2,36 +2,47 @@ import { useState } from "react";
 import Produto from "../../../models/Produto";
 
 function CadastrarProduto() {
-
   const [nome, setNome] = useState("");
+  const [descricao, setDescricao] = useState("");
+  const [quantidade, setQuantidade] = useState(0);
+  const [preco, setPreco] = useState(0);
 
   function enviarProduto(event: any) {
     event.preventDefault();
     submeterProdutoAPI();
   }
 
-  async function submeterProdutoAPI(){
+  async function submeterProdutoAPI() {
     //Biblioteca AXIOS
+    try {
+      const produto: Produto = {
+        nome: nome,
+        descricao: descricao,
+        preco: preco,
+        quantidade: quantidade,
+      };
 
-    const produto : Produto = {
-        nome : nome,
-        descricao : "Teste",
-        preco : 123456,
-        quantidade : 987654
-    };
-
-    const resposta = 
-        await fetch("http://localhost:5011/api/produto/cadastrar", {
-            method : "POST",
-            headers : {
-                "Content-Type" : "application/json"
-            },
-            body : JSON.stringify(produto)
-        });
+      const resposta = await fetch(
+        "http://localhost:5011/api/produto/cadastrar",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(produto),
+        }
+      );
+      if(!resposta.ok){
+        throw new Error("Erro ao cadastrar o protudo: " + resposta.statusText);
+      }
+      console.log(await resposta.json());
+    } catch (error) {
+      console.log(error)
+    }
   }
 
-  function escreverNome(event: any) {
-    setNome(event.target.value);
+  function escreverNome(e: any) {
+    setNome(e.target.value);
   }
   return (
     <div>
@@ -43,7 +54,21 @@ function CadastrarProduto() {
         </div>
         <div>
           <label>Descrição:</label>
-          <input type="text" />
+          <input
+            type="text"
+            onChange={(e: any) => setDescricao(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Quantidade:</label>
+          <input
+            type="text"
+            onChange={(e: any) => setQuantidade(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Preço:</label>
+          <input type="text" onChange={(e: any) => setPreco(e.target.value)} />
         </div>
         <div>
           <button type="submit">Cadastrar</button>
